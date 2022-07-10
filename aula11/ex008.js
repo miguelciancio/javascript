4c22d1e9c8dfcd91d13a19f7232ecfc3ba9c3068
miguelciancio <mciancio.figueiredo@gmail.com>
2022-07-10 12:39:42 +0100

    Aula11, Modulo D - Curso JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var velocidade = 78.2
+
+console.log(`A velocidade do seu carro e ${velocidade}Km/h.`)
+
+if (velocidade > 60) { // Condicao Simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de seguranca!')
+
---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+/* CONDICAO COMPOSTA */
+
+var pais = 'Brasil'
+
+console.log(`Vivendo no(a) ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Voce e Brasileiro!')
+} else {
+    console.log('Voce e Estrangeiro!')
+}
