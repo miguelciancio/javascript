6916b08f7d8a03a0c558c297658c4961b88c6136
miguelciancio <mciancio.figueiredo@gmail.com>
2022-07-21 18:03:21 +0100

    Aula 14, Modulo E - Curso JavaScript

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+console.log('Vai comecar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+/*
+var c = 1
+
+do  {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
