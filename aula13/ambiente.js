b6f029af0826c46b26dacaa632f40c38c71f0a5c
miguelciancio <mciancio.figueiredo@gmail.com>
2022-07-21 17:21:59 +0100

    Aula 13, Modulo E - Curso JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var count = 1
+
+do {
+    console.log(`Passo ${count}`)
+    count++  //  Mesma coisa que count = count + 1
+} while (count <= 6)
+
+ /*
+var count = 1
+
+while (count <= 6) {
+    console.log(`Passo ${count}`)
+    count++  //  Mesma coisa que count = count + 1
+ */
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
